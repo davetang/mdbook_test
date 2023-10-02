4474b57dce0f475adedd6c2e18e82f3dc8b0e938
Dave Tang <davetingpongtang@gmail.com>
2023-10-02 16:22:20 +0900

    Do not track target

--- code/hash_new_from_old/target/debug/deps/hash_new_from_old-25a8c5f0aaba8729.d
+++
@@ -1,5 +0,0 @@
-/home/dtang/github/mdbook_test/code/hash_new_from_old/target/debug/deps/hash_new_from_old-25a8c5f0aaba8729: src/main.rs
-
-/home/dtang/github/mdbook_test/code/hash_new_from_old/target/debug/deps/hash_new_from_old-25a8c5f0aaba8729.d: src/main.rs
-
-src/main.rs:
